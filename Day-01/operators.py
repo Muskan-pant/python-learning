80933abdf9208764ac9ebe502ea83858801a3ec8
unknown <pantm1004@gmail.com>
2026-06-17 00:48:23 +0530

    Day-01 :- Learn variables , Data-Types and Operators

---
+++ Day-01/operators.py
@@ -0,0 +1,63 @@
+# Arithmatic Operator :- + , -, *, / , %, ** 
+a = 10 
+b = 4
+
+print("The sum :", a + b)
+print("The diff :", a - b)
+print("The multiplication :", a * b)
+print("The div :", a / b)
+print("The modulas :", a % b)
+print("The a^b :", a ** b)
+
+
+# Print Sum 
+a = 5 
+b = 8
+sum = a + b 
+print("The sum is :", sum)
+
+# Calculate differance of two numbers .
+c = 5 
+d = 3
+diff = c - d
+print("The differance is :", diff)
+
+
+#Relational/ Comparision Operators :- == , != , > , < , >=, <=
+
+p = 5
+r = 6
+print(p == r) #False 
+print(p != r) #True 
+print(p > r) #False
+print(p < r) #True 
+print(p >= r) #False
+print(p <= r) #True 
+
+#Assignment Operators:- = , += , -= , %= , /= , **= 
+num = 10
+num += 10
+print("Num :", num ) #20
+
+num -= 20
+print(num) #0 
+
+# Logical Operator:- AND (&), OR (||) , NOT(!) 
+#NOT (!) :-  (print( not False)  #True
+print( not True) #False
+
+q = 20
+m  = 4
+print(not (q > m)) #False
+
+#AND (&) :- 
+val1 = True
+val2 = True
+
+print("The value is :", val1 & val2) #True   you can also use and in this 
+
+
+#OR(||):- 
+val3 = True
+val4 = False
+print("The value of OR is :", val3 or val4) #True 
---
+++ Day-01/variables.py
@@ -0,0 +1,15 @@
+name = "Muskan" #string
+age = 20  #integer
+
+CGPA = 8.5  #float
+print(name)
+
+print("My name is :", name)
+print("My Age is :", age)
+print("I got", CGPA , "CGPA")
+
+
+#type() is used to check the Data-type of the variable .
+print(type(name))
+print(type(age))
+print(type(CGPA))
