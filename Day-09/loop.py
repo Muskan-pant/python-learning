fdf53b5c9b6b81a551806e1b10e00f83d6b0f627
unknown <pantm1004@gmail.com>
2026-06-28 21:40:09 +0530

    Lear Range function concept

--- Day-09/loop.py
+++ Day-09/loop.py
@@ -97,4 +97,16 @@ else:
 
 # range() :- returns a sequence of numbers, starting from 0 by default and increments by 1 (by default) and stops before a specified number. 
 # range(start?,stop,step?)
+seq = range(5)
+for i in seq :
+  print(i)
 
+for i in range(2, 10): # range(start,stop) 
+  print(i)
+
+for i in range(2, 11, 3): #range(start,stop,step) 
+  print(i)
+
+
+for i in range(1, 100, 2): 
+  print(i)
